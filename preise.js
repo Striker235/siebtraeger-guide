499f35e25d6e93d5321bb491abdaa8b804a57055
Striker235 <63111061+Striker235@users.noreply.github.com>
2026-09-21 17:34:28 +0200

    Preise

---
+++ preise.js
@@ -0,0 +1,433 @@
+window.SG_PREISE={
+ "stand": "21.09.2026",
+ "produkte": [
+  {
+   "id": "einsteiger-bambino-plus",
+   "kat": "Einsteiger",
+   "name": "Sage Bambino Plus",
+   "note": "4,0",
+   "label": "Testsieger",
+   "thumb": "img/thumb/sage-bambino-plus-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 439.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=einsteiger-bambino-plus&ued=https%3A%2F%2Fwww.roastmarket.de%2Fsage-the-bambino-plus-edelstahl.html",
+     "kauf": true
+    },
+    {
+     "shop": "Amazon",
+     "preis": 479.9,
+     "url": "https://www.amazon.de/dp/B07KYCPZK6?tag=siebtraegergu-21&th=1",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 529.9,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "einsteiger-gastroback-piccolo-pro-m",
+   "kat": "Einsteiger",
+   "name": "Gastroback Design Espresso Piccolo Pro M",
+   "note": "3,8",
+   "label": "Milch-Komfort",
+   "thumb": "img/thumb/gastroback-piccolo-pro-m-studio.png",
+   "angebote": [
+    {
+     "shop": "Amazon",
+     "preis": 216.81,
+     "url": "https://www.amazon.de/dp/B0CJVBR53V?tag=siebtraegergu-21",
+     "kauf": true
+    },
+    {
+     "shop": "roastmarket",
+     "preis": 232.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=einsteiger-gastroback-piccolo-pro-m&ued=https%3A%2F%2Fwww.roastmarket.de%2Fgastroback-design-espresso-piccolo-pro-m.html",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 285.99,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "einsteiger-dedica",
+   "kat": "Einsteiger",
+   "name": "De'Longhi Dedica Style EC685.M",
+   "note": "3,6",
+   "label": "Preis-Leistungssieger",
+   "thumb": "img/thumb/delonghi-dedica-studio.jpg",
+   "angebote": [
+    {
+     "shop": "Amazon",
+     "preis": 123.99,
+     "url": "https://www.amazon.de/dp/B06WGTZ874?tag=siebtraegergu-21",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 154.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=22915&awinaffid=2988869&clickref=einsteiger-dedica&ued=https%3A%2F%2Fwww.delonghi.com%2Fde-de%2Fp%2Fdedica-siebtragermaschinen-dedica-style-siebtragermaschine-ec685.m%2FEC685.M%2BEX%253A4.html%3Fpid%3D0132106266",
+     "kauf": true
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "einsteiger-wmf-lumero",
+   "kat": "Einsteiger",
+   "name": "WMF Lumero Espresso",
+   "note": "3,5",
+   "label": "Geheimtipp",
+   "thumb": "img/thumb/wmf-lumero-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 219.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=einsteiger-wmf-lumero&ued=https%3A%2F%2Fwww.roastmarket.de%2Fwmf-lumero-espresso-siebtraegermaschine.html",
+     "kauf": true
+    },
+    {
+     "shop": "Amazon",
+     "preis": 229.99,
+     "url": "https://www.amazon.de/dp/B07N7S4M4Z?tag=siebtraegergu-21",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 249.0,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "einsteiger-solis-perfetta",
+   "kat": "Einsteiger",
+   "name": "Solis Barista Perfetta Plus",
+   "note": "3,4",
+   "label": null,
+   "thumb": "img/thumb/solis-barista-perfetta-studio.jpg",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 369.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=einsteiger-solis-perfetta&ued=https%3A%2F%2Fwww.roastmarket.de%2Fsolis-barista-perfetta-plus-siebtraegermaschine.html",
+     "kauf": true
+    },
+    {
+     "shop": "Amazon",
+     "preis": 419.9,
+     "url": "https://www.amazon.de/dp/B07ZS664L1?tag=siebtraegergu-21",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 449.9,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "mittelklasse-profitec-go",
+   "kat": "Mittelklasse",
+   "name": "Profitec GO",
+   "note": "4,4",
+   "label": "Testsieger",
+   "thumb": "img/thumb/profitec-go-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 939.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=mittelklasse-profitec-go&ued=https%3A%2F%2Fwww.roastmarket.de%2Fprofitec-go.html%3Fs%3D11640",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 995.0,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": false
+  },
+  {
+   "id": "mittelklasse-barista-pro",
+   "kat": "Mittelklasse",
+   "name": "Sage Barista Pro",
+   "note": "4,3",
+   "label": "Komfort-Tipp",
+   "thumb": "img/thumb/sage-barista-pro-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 589.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=mittelklasse-barista-pro&ued=https%3A%2F%2Fwww.roastmarket.de%2Fsage-the-barista-protm.html",
+     "kauf": true
+    },
+    {
+     "shop": "Amazon",
+     "preis": 639.9,
+     "url": "https://www.amazon.de/dp/B07PY42BRC?tag=siebtraegergu-21",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 829.9,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "mittelklasse-lelit-anna",
+   "kat": "Mittelklasse",
+   "name": "Lelit Anna PL41TEM",
+   "note": "4,2",
+   "label": "Preis-Leistungssieger",
+   "thumb": "img/thumb/lelit-anna-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 569.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=mittelklasse-lelit-anna&ued=https%3A%2F%2Fwww.roastmarket.de%2Flelit-anna-pl41tem-edelstahl.html",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 589.0,
+     "url": null,
+     "kauf": false
+    },
+    {
+     "shop": "Amazon",
+     "preis": 671.85,
+     "url": "https://www.amazon.de/dp/B00BS7RFSO?tag=siebtraegergu-21",
+     "kauf": true
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "mittelklasse-pegaso-pid",
+   "kat": "Mittelklasse",
+   "name": "Quick Mill Pegaso PID",
+   "note": "4,1",
+   "label": "Flow-Tüftler",
+   "thumb": "img/thumb/quick-mill-pegaso-pid-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 999.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=mittelklasse-pegaso-pid&ued=https%3A%2F%2Fwww.roastmarket.de%2Fquick-mill-pegaso-pid.html",
+     "kauf": true
+    },
+    {
+     "shop": "Amazon",
+     "preis": 1016.75,
+     "url": "https://www.amazon.de/dp/B0BSLC7FNP?tag=siebtraegergu-21",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 1150.0,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "mittelklasse-gaggia-classic",
+   "kat": "Mittelklasse",
+   "name": "Gaggia Classic Evo Pro",
+   "note": "4,0",
+   "label": null,
+   "thumb": "img/thumb/gaggia-classic-e24-studio.jpg",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 469.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=mittelklasse-gaggia-classic&ued=https%3A%2F%2Fwww.roastmarket.de%2Fgaggia-classic-evo-pro-e24-inox.html",
+     "kauf": true
+    },
+    {
+     "shop": "Amazon",
+     "preis": 542.08,
+     "url": "https://www.amazon.de/dp/B0CCVY5MSS?tag=siebtraegergu-21",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 549.99,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": false
+  },
+  {
+   "id": "eb-barista-express",
+   "kat": "Mittelklasse",
+   "name": "Sage Barista Express",
+   "note": null,
+   "label": "Erfahrungsbericht",
+   "thumb": "img/thumb/sage-barista-express-studio.jpg",
+   "angebote": [
+    {
+     "shop": "Amazon",
+     "preis": 489.0,
+     "url": "https://www.amazon.de/dp/B07B2X1VXZ?tag=siebtraegergu-21",
+     "kauf": true
+    },
+    {
+     "shop": "roastmarket",
+     "preis": 489.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=eb-barista-express&ued=https%3A%2F%2Fwww.roastmarket.de%2Fsage-the-barista-expresstm.html",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 729.9,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "premium-lelit-bianca",
+   "kat": "Premium",
+   "name": "Lelit Bianca V3",
+   "note": "4,8",
+   "label": "Testsieger",
+   "thumb": "img/thumb/lelit-bianca-v3-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 2299.0,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=premium-lelit-bianca&ued=https%3A%2F%2Fwww.roastmarket.de%2Flelit-bianca.html",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 2499.0,
+     "url": null,
+     "kauf": false
+    },
+    {
+     "shop": "Amazon",
+     "preis": 2795.99,
+     "url": "https://www.amazon.de/dp/B0BB4PT268?tag=siebtraegergu-21",
+     "kauf": true
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "premium-linea-mini-r",
+   "kat": "Premium",
+   "name": "La Marzocco Linea Mini R",
+   "note": "4,7",
+   "label": "Café-Ikone",
+   "thumb": "img/thumb/la-marzocco-linea-mini-r-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 5176.5,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=premium-linea-mini-r&ued=https%3A%2F%2Fwww.roastmarket.de%2Fla-marzocco-linea-mini-r.html",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 5220.0,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": true
+  },
+  {
+   "id": "premium-technika-v",
+   "kat": "Premium",
+   "name": "ECM Technika V Profi PID",
+   "note": "4,7",
+   "label": "Verarbeitung",
+   "thumb": "img/thumb/ecm-technika-v-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 2329.99,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=premium-technika-v&ued=https%3A%2F%2Fwww.roastmarket.de%2Fecm-technika-v-profi-pid-espressomaschine.html",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 2485.0,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": false
+  },
+  {
+   "id": "premium-silvia-pro-x",
+   "kat": "Premium",
+   "name": "Rancilio Silvia Pro X",
+   "note": "4,6",
+   "label": "Kompakter Dualboiler",
+   "thumb": "img/thumb/rancilio-silvia-pro-x-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 1499.0,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=premium-silvia-pro-x&ued=https%3A%2F%2Fwww.roastmarket.de%2Francilio-silvia-pro-x.html",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 2195.0,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": false
+  },
+  {
+   "id": "premium-appartamento",
+   "kat": "Premium",
+   "name": "Rocket Appartamento TCA",
+   "note": "4,5",
+   "label": "Design",
+   "thumb": "img/thumb/rocket-appartamento-tca-studio.png",
+   "angebote": [
+    {
+     "shop": "roastmarket",
+     "preis": 1448.0,
+     "url": "https://www.awin1.com/cread.php?awinmid=16916&awinaffid=2988869&clickref=premium-appartamento&ued=https%3A%2F%2Fwww.roastmarket.de%2Frocket-appartamento-tca.html",
+     "kauf": true
+    },
+    {
+     "shop": "Hersteller",
+     "preis": 1510.0,
+     "url": null,
+     "kauf": false
+    }
+   ],
+   "direkt": false
+  }
+ ]
+};
